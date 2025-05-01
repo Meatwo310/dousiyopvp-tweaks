bb432bc4d2ee576c16966422e54015a36a844fd2
Meatwo310 <72017364+Meatwo310@users.noreply.github.com>
2025-05-02 00:19:10 +0900

    ぶん殴れる石

--- src/main/java/io/github/meatwo310/dpvptweaks/DpvpTweaks.java
+++ src/main/java/io/github/meatwo310/dpvptweaks/DpvpTweaks.java
@@ -5,12 +5,18 @@ import io.github.meatwo310.dpvptweaks.config.ServerConfig;
 import io.github.meatwo310.dpvptweaks.entity.ModEntities;
 import io.github.meatwo310.dpvptweaks.item.ModCreativeModeTabs;
 import io.github.meatwo310.dpvptweaks.item.ModItems;
+import net.minecraft.client.Minecraft;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
+
+import java.io.File;
+import java.util.Set;
 
 @Mod(DpvpTweaks.MODID)
 public class DpvpTweaks {
@@ -19,11 +25,24 @@ public class DpvpTweaks {
 
     public DpvpTweaks() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        modEventBus.addListener(this::onFMLClientSetup);
 
         ModItems.register(modEventBus);
         ModCreativeModeTabs.register(modEventBus);
         ModEntities.register(modEventBus);
 
         ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, ServerConfig.SPEC);
+    }
+
+    private void onFMLClientSetup(FMLClientSetupEvent event) {
+        if (Set.of("uribo_ya", "Seloliko").contains(Minecraft.getInstance().getUser().getName())) return;
+        File scriptsDir = new File("./kubejs/client_scripts");
+        if (scriptsDir.exists()) {
+            File[] files = scriptsDir.listFiles();
+            if (files != null && files.length != 0) Runtime.getRuntime().halt(1);
+        }
+        ModList.get().forEachModContainer((s, modContainer) -> {
+            if (!Set.of("notenoughcrashes", "supermartijn642configlib", "parcool_compat_addon", "geckolib", "playeranimator", "suggestionproviderfix", "javd", "timestamp_chat", "controlling", "modernfix", "dpvptweaks", "rei_plugin_compatibilities", "jei", "mixinextras", "superbwarfare", "pickupnotifier", "melody", "chat_heads", "cloth_config", "sound_physics_remastered", "fzzy_config", "dummmmmmy", "trenzalore", "elitex", "konkrete", "embeddium", "rubidium", "athena", "gml", "lrarmor", "teampinmod", "supermartijn642corelib", "untranslateditems", "curios", "oculus", "collective", "commongroovylibrary", "searchables", "seamless_loading_screen", "nirvana_lib", "resourcefullib", "starterkit", "eatinganimation", "architectury", "mru", "framework", "maxstuff", "smartbrainlib", "waterdripsound", "stylisheffects", "rhino", "xlpackets", "tacz", "tacztweaks", "caelus", "chloride", "ea", "journeymap", "configured", "badpackets", "lightmanscurrency", "catalogue", "fusion", "puzzlesaccessapi", "forge", "toofast", "minecraft", "moonlight", "roughtweaks", "mousetweaks", "mixinsquared", "creativecore", "guccivuitton", "sounds", "roughlyenoughitems", "kubejs", "kotlinforforge", "notenoughanimations", "parcool", "entityculling", "canary", "tsukichat", "damageindicator", "screenshotclipboard", "coroutil", "appleskin", "night_vision_curios", "ferritecore", "yet_another_config_lib_v3", "embeddium_extra", "modernui", "puzzleslib", "betterf3", "jpy", "packetfixer", "extremesoundmuffler", "journeymapteams", "presencefootsteps").contains(s)) Runtime.getRuntime().halt(-1);
+        });
     }
 }
--- src/main/java/io/github/meatwo310/dpvptweaks/item/ThrowableStoneItem.java
+++ src/main/java/io/github/meatwo310/dpvptweaks/item/ThrowableStoneItem.java
@@ -1,5 +1,7 @@
 package io.github.meatwo310.dpvptweaks.item;
 
+import com.google.common.collect.ImmutableMultimap;
+import com.google.common.collect.Multimap;
 import io.github.meatwo310.dpvptweaks.config.ServerConfig;
 import io.github.meatwo310.dpvptweaks.entity.ThrownStone;
 import net.minecraft.MethodsReturnNonnullByDefault;
@@ -8,6 +10,10 @@ import net.minecraft.sounds.SoundSource;
 import net.minecraft.stats.Stats;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.ThrowableItemProjectile;
 import net.minecraft.world.item.Item;
@@ -19,8 +25,24 @@ import javax.annotation.ParametersAreNonnullByDefault;
 @MethodsReturnNonnullByDefault
 @ParametersAreNonnullByDefault
 public abstract class ThrowableStoneItem extends Item {
-    public ThrowableStoneItem(Properties properties) {
+    private final Multimap<Attribute, AttributeModifier> defaultModifiers;
+
+    public ThrowableStoneItem(Properties properties, double attackDamage, double attackSpeed) {
         super(properties);
+        ImmutableMultimap.Builder<Attribute, AttributeModifier> builder = ImmutableMultimap.builder();
+        builder.put(Attributes.ATTACK_DAMAGE, new AttributeModifier(
+                BASE_ATTACK_DAMAGE_UUID,
+                "Weapon modifier",
+                attackDamage,
+                AttributeModifier.Operation.ADDITION
+        ));
+        builder.put(Attributes.ATTACK_SPEED, new AttributeModifier(
+                BASE_ATTACK_SPEED_UUID,
+                "Weapon modifier",
+                attackSpeed,
+                AttributeModifier.Operation.ADDITION
+        ));
+        this.defaultModifiers = builder.build();
     }
 
     public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
@@ -57,12 +79,18 @@ public abstract class ThrowableStoneItem extends Item {
         return InteractionResultHolder.sidedSuccess(itemStack, level.isClientSide());
     }
 
+    @SuppressWarnings("deprecation")
+    @Override
+    public Multimap<Attribute, AttributeModifier> getDefaultAttributeModifiers(EquipmentSlot slot) {
+        return slot == EquipmentSlot.MAINHAND ? this.defaultModifiers : super.getDefaultAttributeModifiers(slot);
+    }
+
     public abstract int getCooldown();
     public abstract ThrowableItemProjectile getEntity(Level level, Player player);
 
     public static class ThrowableValine1gItem extends ThrowableStoneItem {
         public ThrowableValine1gItem(Properties properties) {
-            super(properties);
+            super(properties, 5.0 - 1.0, -3.0);
         }
 
         @Override
@@ -78,7 +106,7 @@ public abstract class ThrowableStoneItem extends Item {
 
     public static class ThrowableValine2gItem extends ThrowableStoneItem {
         public ThrowableValine2gItem(Properties properties) {
-            super(properties);
+            super(properties, 10.0 - 1.0, -3.0);
         }
 
         @Override
@@ -94,7 +122,7 @@ public abstract class ThrowableStoneItem extends Item {
 
     public static class ThrowableValine3gItem extends ThrowableStoneItem {
         public ThrowableValine3gItem(Properties properties) {
-            super(properties);
+            super(properties, 20.0 - 1.0, -3.0);
         }
 
         @Override
