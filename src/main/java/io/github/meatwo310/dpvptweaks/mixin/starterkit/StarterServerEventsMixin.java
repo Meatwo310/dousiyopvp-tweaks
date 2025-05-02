75019e36ea221b448624006dbd2b781918dc69f5
Meatwo310 <72017364+Meatwo310@users.noreply.github.com>
2025-05-02 18:47:23 +0900

    sk

--- src/main/java/io/github/meatwo310/dpvptweaks/mixin/starterkit/StarterServerEventsMixin.java
+++ src/main/java/io/github/meatwo310/dpvptweaks/mixin/starterkit/StarterServerEventsMixin.java
@@ -1,6 +1,7 @@
 package io.github.meatwo310.dpvptweaks.mixin.starterkit;
 
 import com.natamus.starterkit_common_forge.events.StarterServerEvents;
+import io.github.meatwo310.dpvptweaks.config.ServerConfig;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
@@ -15,6 +16,8 @@ public class StarterServerEventsMixin {
             "Lnet/minecraft/world/entity/Entity;" +
             ")V")
     private static void onSpawn(Level level, Entity entity, CallbackInfo ci) {
-        ci.cancel();
+        if (ServerConfig.DISABLE_STARTER_KIT.get()) {
+            ci.cancel();
+        }
     }
 }
