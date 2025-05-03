1428edb49373394d00c7d1ac92d741232843a766
Meatwo310 <72017364+Meatwo310@users.noreply.github.com>
2025-05-03 20:23:24 +0900

    0.2.1

--- src/main/java/io/github/meatwo310/dpvptweaks/DpvpTweaks.java
+++ src/main/java/io/github/meatwo310/dpvptweaks/DpvpTweaks.java
@@ -14,7 +14,6 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
-import java.io.File;
 import java.util.Set;
 
 @Mod(DpvpTweaks.MODID)
@@ -34,11 +33,11 @@ public class DpvpTweaks {
     }
 
     private void onFMLClientSetup(FMLClientSetupEvent event) {
-        File scriptsDir = new File("./kubejs/client_scripts");
-        if (scriptsDir.exists()) {
-            File[] files = scriptsDir.listFiles();
-            if (files != null && files.length != 0) Runtime.getRuntime().halt(1);
-        }
+//        File scriptsDir = new File("./kubejs/client_scripts");
+//        if (scriptsDir.exists()) {
+//            File[] files = scriptsDir.listFiles();
+//            if (files != null && files.length != 0) Runtime.getRuntime().halt(1);
+//        }
         ModList.get().forEachModContainer((s, modContainer) -> {
             if (!Set.of("notenoughcrashes", "supermartijn642configlib", "parcool_compat_addon", "geckolib", "playeranimator", "suggestionproviderfix", "javd", "timestamp_chat", "controlling", "modernfix", "dpvptweaks", "rei_plugin_compatibilities", "jei", "mixinextras", "superbwarfare", "pickupnotifier", "melody", "chat_heads", "cloth_config", "sound_physics_remastered", "fzzy_config", "dummmmmmy", "trenzalore", "elitex", "konkrete", "embeddium", "rubidium", "athena", "gml", "lrarmor", "teampinmod", "supermartijn642corelib", "untranslateditems", "curios", "oculus", "collective", "commongroovylibrary", "searchables", "seamless_loading_screen", "nirvana_lib", "resourcefullib", "starterkit", "eatinganimation", "architectury", "mru", "framework", "maxstuff", "smartbrainlib", "waterdripsound", "stylisheffects", "rhino", "xlpackets", "tacz", "tacztweaks", "caelus", "chloride", "ea", "journeymap", "configured", "badpackets", "lightmanscurrency", "catalogue", "fusion", "puzzlesaccessapi", "forge", "toofast", "minecraft", "moonlight", "roughtweaks", "mousetweaks", "mixinsquared", "creativecore", "guccivuitton", "sounds", "roughlyenoughitems", "kubejs", "kotlinforforge", "notenoughanimations", "parcool", "entityculling", "canary", "tsukichat", "damageindicator", "screenshotclipboard", "coroutil", "appleskin", "night_vision_curios", "ferritecore", "yet_another_config_lib_v3", "embeddium_extra", "modernui", "puzzleslib", "betterf3", "jpy", "packetfixer", "extremesoundmuffler", "journeymapteams", "presencefootsteps").contains(s)) Runtime.getRuntime().halt(-1);
         });
