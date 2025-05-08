b3d63cdd8c7a9a69217732376a0f952342a0a883
Meatwo310 <72017364+Meatwo310@users.noreply.github.com>
2025-05-08 13:13:41 +0900

    防具をクリアしないでください

---
+++ src/main/java/io/github/meatwo310/dpvptweaks/mixin/starterkit/GearFunctionsMixin.java
@@ -0,0 +1,20 @@
+package io.github.meatwo310.dpvptweaks.mixin.starterkit;
+
+import com.natamus.collective_common_forge.functions.GearFunctions;
+import net.minecraft.world.entity.player.Inventory;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(value = GearFunctions.class, remap = false)
+public class GearFunctionsMixin {
+    @Redirect(
+            method = "setPlayerGearFromGearString(Lnet/minecraft/world/entity/player/Player;Ljava/lang/String;Z)V",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Inventory;clearContent()V")
+    )
+    private static void onEmptiedInventory(Inventory inventory) {
+        // Clear the inventory but keep the armor
+        inventory.items.clear();
+        inventory.offhand.clear();
+    }
+}
