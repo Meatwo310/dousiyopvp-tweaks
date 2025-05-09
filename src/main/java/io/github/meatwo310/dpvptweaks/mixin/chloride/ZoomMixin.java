cb3c5d832b4d2557aa56837d930e273b096c9eb4
Meatwo310 <72017364+Meatwo310@users.noreply.github.com>
2025-05-09 22:34:27 +0900

    Chlorideをアップデート

--- src/main/java/io/github/meatwo310/dpvptweaks/mixin/chloride/ZoomMixin.java
+++ src/main/java/io/github/meatwo310/dpvptweaks/mixin/chloride/ZoomMixin.java
@@ -1,11 +1,11 @@
 package io.github.meatwo310.dpvptweaks.mixin.chloride;
 
-import me.srrapero720.chloride.features.ZoomFeature;
+import me.srrapero720.chloride.impl.Zoom;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 
-@Mixin(value = ZoomFeature.class, remap = false)
-public class ZoomFeatureMixin {
+@Mixin(value = Zoom.class, remap = false)
+public class ZoomMixin {
     /**
      * @author Meatwo310
      * @reason Blocks zoom feature
