ac76d2ae7789d43644a146eda1de4350eb66b33c
Meatwo310 <72017364+Meatwo310@users.noreply.github.com>
2025-05-29 11:51:23 +0900

    もーーーーーー

--- src/main/java/io/github/meatwo310/dpvptweaks/mixin/lightmanscurrency/CommandPlayerTradingMixin.java
+++ src/main/java/io/github/meatwo310/dpvptweaks/mixin/lightmanscurrency/CommandPlayerTradingMixin.java
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(CommandPlayerTrading.class)
+@Mixin(value = CommandPlayerTrading.class, remap = false)
 public class CommandPlayerTradingMixin {
     @Inject(
             method = "requestPlayerTrade",
