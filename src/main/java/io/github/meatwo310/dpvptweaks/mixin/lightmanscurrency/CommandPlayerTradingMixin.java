4d09caaf4ee6213a1b12382fc63a173baa6c5d5f
Meatwo310 <72017364+Meatwo310@users.noreply.github.com>
2025-05-29 11:15:33 +0900

    取引禁止

---
+++ src/main/java/io/github/meatwo310/dpvptweaks/mixin/lightmanscurrency/CommandPlayerTradingMixin.java
@@ -0,0 +1,29 @@
+package io.github.meatwo310.dpvptweaks.mixin.lightmanscurrency;
+
+import com.mojang.brigadier.Command;
+import com.mojang.brigadier.context.CommandContext;
+import io.github.lightman314.lightmanscurrency.common.commands.CommandPlayerTrading;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.network.chat.Component;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(CommandPlayerTrading.class)
+public class CommandPlayerTradingMixin {
+    @Inject(
+            method = "requestPlayerTrade",
+            at = @At(value = "INVOKE",
+                    target = "Lio/github/lightman314/lightmanscurrency/common/playertrading/PlayerTradeManager;" +
+                            "CreateNewTrade(" +
+                            "Lnet/minecraft/server/level/ServerPlayer;" +
+                            "Lnet/minecraft/server/level/ServerPlayer;" +
+                            ")I"),
+            cancellable = true
+    )
+    private static void onCreateNewTrade(CommandContext<CommandSourceStack> ctx, CallbackInfoReturnable<Integer> cir) {
+        ctx.getSource().sendFailure(Component.literal("ﾇﾍﾞｰｯ!"));
+        cir.setReturnValue(0);
+    }
+}
