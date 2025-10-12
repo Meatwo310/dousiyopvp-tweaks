e161382b6320094eef22f3068560f7a29582ebb6
Meatwo310 <72017364+Meatwo310@users.noreply.github.com>
2025-10-12 19:03:58 +0900

    もっとまともなコンフィグ変更コマンドが作りたいです

--- src/main/java/io/github/meatwo310/dpvptweaks/command/DpvpTweaksConfigCommand.java
+++ src/main/java/io/github/meatwo310/dpvptweaks/command/DpvpTweaksConfigCommand.java
@@ -1,6 +1,7 @@
 package io.github.meatwo310.dpvptweaks.command;
 
 import com.mojang.brigadier.Command;
+import com.mojang.brigadier.arguments.BoolArgumentType;
 import com.mojang.brigadier.arguments.FloatArgumentType;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
@@ -15,9 +16,9 @@ public class DpvpTweaksConfigCommand {
     static void register(LiteralArgumentBuilder<CommandSourceStack> builder, RegisterCommandsEvent event) {
         builder.then(Commands.literal("config")
                 .requires(s -> s.hasPermission(2))
-                .then(DpvpTweaksConfigCommand.createConfigCommand("valine1g", ServerConfig.VALINE1G_DAMAGE, ServerConfig.VALINE1G_COOLDOWN))
-                .then(DpvpTweaksConfigCommand.createConfigCommand("valine2g", ServerConfig.VALINE2G_DAMAGE, ServerConfig.VALINE2G_COOLDOWN))
-                .then(DpvpTweaksConfigCommand.createConfigCommand("valine3g", ServerConfig.VALINE3G_DAMAGE, ServerConfig.VALINE3G_COOLDOWN))
+                .then(valineCommand("valine1g", ServerConfig.VALINE1G_DAMAGE, ServerConfig.VALINE1G_COOLDOWN))
+                .then(valineCommand("valine2g", ServerConfig.VALINE2G_DAMAGE, ServerConfig.VALINE2G_COOLDOWN))
+                .then(valineCommand("valine3g", ServerConfig.VALINE3G_DAMAGE, ServerConfig.VALINE3G_COOLDOWN))
                 .executes(ctx -> {
                     String message = """
                             Current config:
@@ -37,17 +38,23 @@ public class DpvpTweaksConfigCommand {
                     ctx.getSource().sendSuccess(() -> Component.literal(message), false);
                     return Command.SINGLE_SUCCESS;
                 })
+                .then(Commands.literal("irons")
+                        .then(config("overrideAttributes", ServerConfig.IRONS_OVERRIDE_ATTRIBUTES))
+                        .then(config("overrideManaRegen", ServerConfig.IRONS_OVERRIDE_MANA_REGEN))
+                        .then(config("manaRegenValue", ServerConfig.IRONS_MANA_REGEN_VALUE))
+                        .then(config("maxMana", ServerConfig.IRONS_MAX_MANA))
+                )
         );
     }
 
-    private static LiteralArgumentBuilder<CommandSourceStack> createConfigCommand(String name, ForgeConfigSpec.ConfigValue<Double> damageConfig, ForgeConfigSpec.IntValue cooldownConfig) {
+    private static LiteralArgumentBuilder<CommandSourceStack> valineCommand(String name, ForgeConfigSpec.ConfigValue<Double> damageConfig, ForgeConfigSpec.IntValue cooldownConfig) {
         return Commands.literal(name)
                 .requires(commandSourceStack -> commandSourceStack.hasPermission(2))
-                .then(registerCommand("damage", damageConfig))
-                .then(registerCommand("cooldown", cooldownConfig));
+                .then(config("damage", damageConfig))
+                .then(config("cooldown", cooldownConfig));
     }
 
-    private static LiteralArgumentBuilder<CommandSourceStack> registerCommand(String name, ForgeConfigSpec.ConfigValue<Double> configValue) {
+    private static LiteralArgumentBuilder<CommandSourceStack> config(String name, ForgeConfigSpec.ConfigValue<Double> configValue) {
         return Commands.literal(name)
                 .then(Commands.literal("reset")
                         .executes(context -> {
@@ -71,7 +78,7 @@ public class DpvpTweaksConfigCommand {
                 });
     }
 
-    private static LiteralArgumentBuilder<CommandSourceStack> registerCommand(String name, ForgeConfigSpec.IntValue configValue) {
+    private static LiteralArgumentBuilder<CommandSourceStack> config(String name, ForgeConfigSpec.IntValue configValue) {
         return Commands.literal(name)
                 .then(Commands.literal("reset")
                         .executes(context -> {
@@ -84,6 +91,30 @@ public class DpvpTweaksConfigCommand {
                 .then(Commands.argument("value", IntegerArgumentType.integer(0))
                         .executes(context -> {
                             int value = IntegerArgumentType.getInteger(context, "value");
+                            configValue.set(value);
+                            context.getSource().sendSuccess(() -> Component.literal(name + " set to: " + value), false);
+                            return Command.SINGLE_SUCCESS;
+                        })
+                )
+                .executes(context -> {
+                    context.getSource().sendSuccess(() -> Component.literal(name + ": " + configValue.get()), false);
+                    return Command.SINGLE_SUCCESS;
+                });
+    }
+
+    private static LiteralArgumentBuilder<CommandSourceStack> config(String name, ForgeConfigSpec.BooleanValue configValue) {
+        return Commands.literal(name)
+                .then(Commands.literal("reset")
+                        .executes(context -> {
+                            boolean previousValue = configValue.get();
+                            configValue.set(configValue.getDefault());
+                            context.getSource().sendSuccess(() -> Component.literal(name + " with value: " + previousValue + " reset to default: " + configValue.get()), false);
+                            return Command.SINGLE_SUCCESS;
+                        })
+                )
+                .then(Commands.argument("value", BoolArgumentType.bool())
+                        .executes(context -> {
+                            boolean value = BoolArgumentType.getBool(context, "value");
                             configValue.set(value);
                             context.getSource().sendSuccess(() -> Component.literal(name + " set to: " + value), false);
                             return Command.SINGLE_SUCCESS;
